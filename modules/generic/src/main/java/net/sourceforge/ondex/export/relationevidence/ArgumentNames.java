f250dfaee951cd93a55391028eed59090345120e
Jan.Taubert@kws.com <Jan.Taubert@kws.com>
2013-01-01 18:58:25 +0000

    tested relation evidence statistic export
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/trunk@18950 3e652917-321c-4c12-a74f-5ef3cffe31a5

--- modules/generic/src/main/java/net/sourceforge/ondex/export/relationevidence/ArgumentNames.java
+++ modules/generic/src/main/java/net/sourceforge/ondex/export/relationevidence/ArgumentNames.java
@@ -11,9 +11,5 @@ public final class ArgumentNames {
 	public static String EVIDENCE_ARG = "Evidence";
 
 	public static String EVIDENCE_ARG_DESC = "This option provides a list of evidence types to include in the statistics.";
-	
-	public static String OUTPUT_ARG = "Output";
-	
-	public static String OUTPUT_ARG_DESC = "This option specifies the path of the output file.";
 
 }
