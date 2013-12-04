f1aa0c8cebafd0b091525ac274972cb8ed12cb25
lysenkoa <lysenkoa@users.sf.net>
2013-12-04 16:51:59 +0000

    Added developer-use option for future reference
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/trunk@19129 3e652917-321c-4c12-a74f-5ef3cffe31a5

--- apps/ovtk2/src/main/java/net/sourceforge/ondex/ovtk2/Main.java
+++ apps/ovtk2/src/main/java/net/sourceforge/ondex/ovtk2/Main.java
@@ -1,9 +1,13 @@
 package net.sourceforge.ondex.ovtk2;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 import javax.swing.JOptionPane;
 
+import net.sourceforge.ondex.config.Config;
+import net.sourceforge.ondex.init.PluginRegistry;
 import net.sourceforge.ondex.ovtk2.ui.OVTK2Desktop;
 import net.sourceforge.ondex.ovtk2.util.DesktopUtils;
 
@@ -20,9 +24,14 @@ public class Main {
 	 * 
 	 * @param args
 	 *            the program arguments
+	 * @throws URISyntaxException 
+	 * @throws IOException 
 	 * @throws Exception
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		/* uncomment and use these lines to switch beteen loading plugins from .jar files or classpath */
+    	//System.setProperty("plugin.scan.lib", Boolean.TRUE.toString());
+    	//PluginRegistry.init(true, "", Config.libDir);
 		long heapMaxSize = Runtime.getRuntime().maxMemory();
 		if (heapMaxSize < 1037959168) {
 			JOptionPane
