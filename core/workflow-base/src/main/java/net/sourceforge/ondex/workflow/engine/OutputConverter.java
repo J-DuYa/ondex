e660d75141f9e04e4ced279866980fa8ca3511bd
lysenkoa <lysenkoa@users.sf.net>
2013-12-17 16:56:00 +0000

    updating tag
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/tags/0.5.0@19176 3e652917-321c-4c12-a74f-5ef3cffe31a5

--- core/workflow-base/src/main/java/net/sourceforge/ondex/workflow/engine/OutputConverter.java
+++
@@ -1,7 +0,0 @@
-package net.sourceforge.ondex.workflow.engine;
-
-import org.jdom.Element;
-
-public interface OutputConverter {
-    public Element convert(Object o);
-}
--- webservices/restful/startup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh -e
-nohup /usr/java/latest/bin/java -Xmx2048M -jar restful.jar graphs >> stdout.log 2>> stderr.log &
