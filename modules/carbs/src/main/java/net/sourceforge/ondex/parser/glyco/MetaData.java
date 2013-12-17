d267018388ae83d839000bb95dd6a849ef2bd921
lysenkoa <lysenkoa@users.sf.net>
2013-12-17 17:28:24 +0000

    fixing directory structure
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/tags/0.5.0@19183 3e652917-321c-4c12-a74f-5ef3cffe31a5

--- modules/carbs/src/main/java/net/sourceforge/ondex/parser/glyco/MetaData.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.ondex.parser.glyco;
-
-public interface MetaData {
-
-	public final static String ET = "IMPD";
-	
-	public final static String CV = "GLYCODB";
-	
-}
--- modules/enzymatics/src/main/java/net/sourceforge/ondex/parser/brenda/ArgumentNames.java
+++
@@ -1,8 +0,0 @@
-package net.sourceforge.ondex.parser.brenda;
-
-public interface ArgumentNames {
-
-	public static final String SPECIES_ARG = "Species";
-	public static final String SPECIES_ARG_DESC = "Species TaxId from NCBI prefered";
-	
-}
