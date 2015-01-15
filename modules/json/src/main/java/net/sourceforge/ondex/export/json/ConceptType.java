dbaf873e71835312d8995e8e91b997273cb4198b
AjitPS <ajitprataps@yahoo.com>
2015-01-15 17:53:57 +0000

    cmt02- JSON Exporter plugin module added.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/branches/Jan15@19215 3e652917-321c-4c12-a74f-5ef3cffe31a5

---
+++ modules/json/src/main/java/net/sourceforge/ondex/export/json/ConceptType.java
@@ -0,0 +1,11 @@
+package net.sourceforge.ondex.export.json;
+
+/**
+ * @author Ajit Singh
+ * @version 05/01/15
+ */
+public enum ConceptType {
+ Gene, Protein, Compound, Cellular_Component, Pathway, Reaction, Enzyme, Enzyme_Classification, 
+ Phenotype, Publication, Biological_Process, Molecular_Function
+ // , Protein_Domain, Trait_Ontology
+}
