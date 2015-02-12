d8f10a274de1dcc5a94b95ae29cac30f05da01bc
AjitPS <ajitprataps@yahoo.com>
2015-02-12 14:14:20 +0000

    cmt05- tested (stable) jsonIO plugin code added
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/branches/Feb2015@19220 3e652917-321c-4c12-a74f-5ef3cffe31a5

---
+++ modules/json/src/main/java/net/sourceforge/ondex/export/json/AddRelationEdgeInfo.java
@@ -0,0 +1,68 @@
+package net.sourceforge.ondex.export.json;
+
+import net.sourceforge.ondex.core.ONDEXRelation;
+import org.json.simple.JSONObject;
+
+/**
+ * Build edge json objects using their various attributes.
+ * @author Ajit Singh
+ * @version 09/01/15
+ */
+public class AddRelationEdgeInfo {
+    
+ public JSONObject getEdgeJson(ONDEXRelation rel) {
+  JSONObject edge= new JSONObject();
+  JSONObject edgeData= new JSONObject();
+
+  String relationID= String.valueOf(rel.getId()); // relation ID.
+  String sourceConcept= String.valueOf(rel.getFromConcept().getId()); // relation source ID.
+  String targetConcept= String.valueOf(rel.getToConcept().getId()); // relation target ID.
+  String edgeLabel= rel.getOfType().getFullname(); // relation type label.
+
+  edgeData.put(/*"id"*/JSONAttributeNames.ID, "e"+relationID);
+  edgeData.put("source", sourceConcept);
+  edgeData.put("target", targetConcept);
+  edgeData.put("label", edgeLabel);
+  
+  // Set the edge color for this Relation.
+  String edgeColour= determineEdgeColour(edgeLabel);
+  edgeData.put("edgeColor", edgeColour);
+
+  edge.put("data", edgeData); // the edge's data.
+  edge.put("group", "edges"); // Grouping edges together
+
+  return edge;
+ }
+
+ private String determineEdgeColour(String edgeLbl) {
+  String colour= EdgeColour.grey.toString(); // default.
+
+  // Determine the colour for this relation based on the edge label.
+  if(edgeLbl.equals(EdgeLabel.published_in.toString())) {
+     colour= EdgeColour.orange.toString();
+    }
+  else if(edgeLbl.equals(EdgeLabel.encodes.toString())) {
+     colour= EdgeColour.grey.toString();
+    }
+  else if(edgeLbl.equals(EdgeLabel.participates_in.toString())) {
+     colour= EdgeColour.teal.toString();
+    }
+  else if(edgeLbl.equals(EdgeLabel.has_similar_sequence.toString())) {
+     colour= EdgeColour.red.toString();
+    }
+  else if((edgeLbl.equals(EdgeLabel.has_protein_domain.toString())) || 
+          (edgeLbl.equals(EdgeLabel.interacts_with.toString())) || 
+          (edgeLbl.equals(EdgeLabel.cooccurs_with.toString()))) {
+     colour= EdgeColour.blue.toString();
+    }
+  else if(edgeLbl.equals(EdgeLabel.located_in.toString())) {
+     colour= EdgeColour.lightGreen.toString();
+    }
+  else if(edgeLbl.equals(EdgeLabel.has_function.toString())) {
+     colour= EdgeColour.purple.toString();
+    }
+
+  return colour;
+ }
+
+}
---
+++ modules/json/src/main/java/net/sourceforge/ondex/export/json/ConceptVisibility.java
@@ -0,0 +1,9 @@
+package net.sourceforge.ondex.export.json;
+
+/**
+ * @author Ajit Singh
+ * @version 16/12/14
+ */
+public enum ConceptVisibility {
+ element, none
+}
