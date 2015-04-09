95e9baea01cc1103128a4670f9cd9477bd03a6e1
AjitPS <ajitprataps@yahoo.com>
2015-04-09 09:13:49 +0000

    modules_new cyjs_Json plugin
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/trunk@19243 3e652917-321c-4c12-a74f-5ef3cffe31a5

---
+++ modules/cyjs_json/src/main/java/net/sourceforge/ondex/export/cyjsJson/ConceptColour.java
@@ -0,0 +1,9 @@
+package net.sourceforge.ondex.export.cyjsJson;
+
+/**
+ * @author Ajit Singh
+ * @version 16/12/14
+ */
+public enum ConceptColour {
+ red, yellow, orange, cyan, teal, grey, lightGreen, lightRed, purple, pink
+}
