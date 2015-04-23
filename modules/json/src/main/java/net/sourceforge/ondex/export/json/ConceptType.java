30784264fe8a25fd444b6a35ea3f71988f69f2ea
AjitPS <ajitprataps@yahoo.com>
2015-04-23 11:47:23 +0000

    jsonIO_plugin_concept_visual_Attributes_modified
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/trunk@19267 3e652917-321c-4c12-a74f-5ef3cffe31a5

--- modules/json/src/main/java/net/sourceforge/ondex/export/json/ConceptType.java
+++ modules/json/src/main/java/net/sourceforge/ondex/export/json/ConceptType.java
@@ -2,10 +2,10 @@ package net.sourceforge.ondex.export.json;
 
 /**
  * @author Ajit Singh
- * @version 05/01/15
+ * @version 23/04/15
  */
 public enum ConceptType {
  Gene, Protein, Compound, SNP, Cellular_Component, Pathway, Reaction, Enzyme, Enzyme_Classification, 
  Phenotype, Publication, Biological_Process, Molecular_Function
- // , Protein_Domain, Trait_Ontology, Quantitative Trait Locus, Enzyme Classification
+ // , Protein Domain, Trait Ontology, Quantitative Trait Locus, Enzyme Classification
 }
