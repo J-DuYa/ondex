8c4262b12a8ba175d1578a4391fb586eff4c9541
AjitPS <ajitprataps@yahoo.com>
2015-06-17 07:49:37 +0000

    concept type-default- changed from Gene to Phenotype for blank data
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/branches/Apr15@19302 3e652917-321c-4c12-a74f-5ef3cffe31a5

--- modules/json/src/main/java/net/sourceforge/ondex/export/json/AddConceptNodeInfo.java
+++ modules/json/src/main/java/net/sourceforge/ondex/export/json/AddConceptNodeInfo.java
@@ -115,8 +115,8 @@ public class AddConceptNodeInfo {
 
  private String[] determineNodeColourAndShape(String conType) {
   String[] attr= new String[2];
-  String shape= ConceptShape.triangle.toString(); // default (for concept Type: 'Gene').
-  String colour= ConceptColour.lightBlue.toString(); // default (for concept Type: 'Gene').
+  String shape= ConceptShape.rectangle.toString(); // default (for concept Type: 'Phenotype').
+  String colour= ConceptColour.greenYellow.toString(); // default (for concept Type: 'Phenotype').
 
   // Determine the shape & colour attributes for this concept based on the concept type.
   if(conType.equals(ConceptType.Biological_Process.toString())) {
