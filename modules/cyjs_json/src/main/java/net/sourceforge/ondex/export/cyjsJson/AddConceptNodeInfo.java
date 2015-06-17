28eb7740135a810cec0eb5ef4e0525876fa7ab78
AjitPS <ajitprataps@yahoo.com>
2015-06-17 08:13:20 +0000

    concept type reset to Gene (for now)
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/trunk@19306 3e652917-321c-4c12-a74f-5ef3cffe31a5

--- modules/cyjs_json/src/main/java/net/sourceforge/ondex/export/cyjsJson/AddConceptNodeInfo.java
+++ modules/cyjs_json/src/main/java/net/sourceforge/ondex/export/cyjsJson/AddConceptNodeInfo.java
@@ -31,9 +31,9 @@ public class AddConceptNodeInfo {
        }
     }
   String conceptType= con.getOfType().getFullname(); // conceptType.
-  if(conceptType.equals("")) {
+/*  if(conceptType.equals("")) {
      conceptType= ConceptType.Phenotype.toString(); // default.
-    }
+    }*/
 
   // For concept Type: "SNP".
   if(conceptType.equalsIgnoreCase(ConceptType.Compound.toString())) {
@@ -115,8 +115,10 @@ public class AddConceptNodeInfo {
 
  private String[] determineNodeColourAndShape(String conType) {
   String[] attr= new String[2];
-  String shape= ConceptShape.rectangle.toString(); // default (for concept Type: 'Phenotype').
-  String colour= ConceptColour.greenYellow.toString(); // default (for concept Type: 'Phenotype').
+//  String shape= ConceptShape.rectangle.toString(); // default (for concept Type: 'Phenotype').
+//  String colour= ConceptColour.greenYellow.toString(); // default (for concept Type: 'Phenotype').
+  String shape= ConceptShape.triangle.toString(); // default (for concept Type: 'Gene').
+  String colour= ConceptColour.blue.toString(); // default (for concept Type: 'Gene').
 
   // Determine the shape & colour attributes for this concept based on the concept type.
   if(conType.equals(ConceptType.Biological_Process.toString())) {
