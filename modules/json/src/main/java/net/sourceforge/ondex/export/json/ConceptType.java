a1c65e30df90aaa02ba528e09aea060c49a29ca0
AjitPS <ajitprataps@yahoo.com>
2015-10-14 12:02:16 +0000

    jsonIO_added_new_CC_and_RT
    git-svn-id: svn+ssh://svn.code.sf.net/p/ondex/code/branches/Apr15@19325 3e652917-321c-4c12-a74f-5ef3cffe31a5

--- modules/json/src/main/java/net/sourceforge/ondex/export/json/ConceptType.java
+++ modules/json/src/main/java/net/sourceforge/ondex/export/json/ConceptType.java
@@ -6,7 +6,7 @@ package net.sourceforge.ondex.export.json;
  */
 public enum ConceptType {
  Gene, Protein, Compound, SNP, Cellular_Component, Pathway, Reaction, Enzyme, Enzyme_Classification, 
- Phenotype, Publication, Biological_Process, Molecular_Function, Scaffold, Chromosome, RNA, Protein_Complex,
- Transport
+ Phenotype, Publication, Biological_Process, Molecular_Function, Scaffold, Trait, Chromosome, RNA, 
+ Protein_Complex, Transport
  // , Protein Domain, Trait Ontology, Quantitative Trait Locus, Enzyme Classification, Protein Complex
 }
