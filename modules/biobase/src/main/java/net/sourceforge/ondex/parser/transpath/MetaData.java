e394d45c6267a67918a6746908b64beeba9d75ff
Marco Brandizi <marco.brandizi@gmail.com>
2017-02-10 14:08:19 +0000

    Test for codebase rearrangement.

--- modules/biobase/src/main/java/net/sourceforge/ondex/parser/transpath/MetaData.java
+++
@@ -1,8 +0,0 @@
-package net.sourceforge.ondex.parser.transpath;
-
-public class MetaData {
-
-	public final static String UPROTKB	= "UNIPROTKB";
-	public final static String IPRO 	= "IPRO"; //interpro
-	
-}
--- modules/clustering/src/main/java/net/sourceforge/ondex/transformer/buildhierarchy/ArgumentNames.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.ondex.transformer.buildhierarchy;
-
-public interface ArgumentNames extends net.sourceforge.ondex.transformer.ArgumentNames {
-	
-	public static final String TAXID_ARG = "TaxID";
-	
-	public static final String TAXID_ARG_DESC = "Taxonomy ID restriction";
-	
-}
